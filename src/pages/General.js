b545f3428c6baf2fc34daa524ade992491dbaf79
Alex Barnsley <8069294+alexbarnsley@users.noreply.github.com>
2019-02-18 19:35:50 +0000

    feat: test require local file

--- src/pages/General.js
+++ src/pages/General.js
@@ -1,9 +1,12 @@
+const add = require('../utils/add')
+
 module.exports = {
   template: `
     <div>
       <h1>General</h1>
       <div>Computed: {{ testComputed }}</div>
       <div>Computed Trigger: {{ showThisComputed }}</div>
+      <div>Require Local: {{ requireLocal }}</div>
 
       <button v-if="!showThis" @click="triggerShowThis">Trigger</button>
     </div>
@@ -22,6 +25,14 @@ module.exports = {
 
     showThisComputed () {
       return this.showThis === null ? '-' : (this.showThis ? 'working' : 'not working')
+    },
+
+    requireLocal () {
+      try {
+        return add(1, 2) === 3 ? 'working' : 'not working'
+      } catch (error) {
+        return 'not working'
+      }
     }
   },
 
